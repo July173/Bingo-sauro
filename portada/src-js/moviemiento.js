75b517f9c612376e30d07e59ad93b48b813b2eee
JULY <danielitha345ra@gmail.com>
2024-09-03 07:33:28 -0500

    portada establecida corecctamente

--- portada/src-js/moviemiento.js
+++ portada/src-js/moviemiento.js
@@ -1,5 +1,10 @@
 function cambiarFondo() {
     document.body.classList.add('show-background');
+    
+    // Espera 3 segundos después del cambio de fondo para mostrar los elementos de gifts
+    setTimeout(function() {
+        document.body.classList.add('show-gifts');
+    }, 2000);
 }
 
 // Cambiar el fondo después de 2 segundos (2000 ms)
