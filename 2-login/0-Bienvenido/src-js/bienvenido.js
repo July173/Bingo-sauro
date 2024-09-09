374a465f28d0d6602359fc0376b3deea4f479905
JULY <danielitha345ra@gmail.com>
2024-09-09 08:36:31 -0500

    redirigiemientos hechos en todas las pantallas,solo falta del iniciar al home y falta hacer validaciones de formularios

--- 2-login/0-Bienvenido/src-js/bienvenido.js
+++ 2-login/0-Bienvenido/src-js/bienvenido.js
@@ -1,6 +1,7 @@
-document.getElementById('redirigirCodigo').addEventListener('click', function(){
-    window.location.href = "/2-login/4-codigoCorreo/contracodigo.html"
-});
 document.getElementById('redirigirIniciar').addEventListener('click', function(){
     window.location.href = "/2-login/1-inicioSesion/InicioSesion.html"
+});
+
+document.getElementById('redirigirRegistrar').addEventListener('click', function(){
+    window.location.href = "/2-login/2-registro/registro.html"
 });
---
+++ 2-login/4-codigoCorreo/src-js/codigo.js
@@ -0,0 +1,6 @@
+document.getElementById('redirigirOlvidar').addEventListener('click', function(){
+    window.location.href = "/2-login/3-forgotPassword/forgotpassword.html"
+});
+document.getElementById('redirigirCambiar').addEventListener('click', function(){
+    window.location.href = "/2-login/5-CambiarPassword/cambiarContra.html"
+});
