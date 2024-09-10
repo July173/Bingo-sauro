250e31ca8abd6e9a02db3221b9a22f290557e927
JULY <danielitha345ra@gmail.com>
2024-09-09 19:17:51 -0500

    carpetas mejor estructuras faltas algunos cambios (hojas).

---
+++ login/CambiarPassword/src-js/cambiar.js
@@ -0,0 +1,6 @@
+document.getElementById('redirigirCodigo').addEventListener('click', function(){
+    window.location.href = "/login/codigoCorreo/contracodigo.html"
+});
+document.getElementById('redirigirIniciar').addEventListener('click', function(){
+    window.location.href = "/login/inicioSesion/InicioSesion.html"
+});
