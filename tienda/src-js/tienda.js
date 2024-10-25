f9ca18d6b3f49cfb87ca28cb627b494b89871edb
Sebastian Penna <sebastianpennaariss@gmail.com>
2024-10-25 17:24:37 -0500

    cambios nuevos y arrglo de las imagenes de las monedas

--- tienda/src-js/tienda.js
+++ tienda/src-js/tienda.js
@@ -3,25 +3,40 @@ fetch('../tienda/comprarCarton.json')
   .then(response => response.json())
   .then(data => {
     const contenedorCartones = document.getElementById('contenedorCartones');
-    
+
     data.cartones.forEach(carton => {
       // Crear un contenedor para cada carton
       const cartonDiv = document.createElement('div');
       cartonDiv.classList.add('carton-item');
-      cartonDiv.innerHTML = `<img src="${carton.src}" alt="${carton.alt}" class="${carton.locked ? 'carton' : 'carton comprar'}">`;
 
-        const precioBoton = document.createElement('button');
+      if (carton.locked) {
+        cartonDiv.innerHTML = `
+         <div class="avatar-wrapper">
+        <img src="${carton.src}" alt="${carton.alt}" class="carton comprar">`;
+      }
+      else {
+        // Si no está bloqueado, solo mostrar la imagen
+        cartonDiv.innerHTML = `<img src="${carton.src}" alt="${carton.alt}" class="carton comprado">`;
+      }
+
+      const precioBoton = document.createElement('button');
+
+      if (carton.locked) {
         precioBoton.textContent = ` ${carton.price} `;
         precioBoton.classList.add('price-button');
+        precioBoton.addEventListener('click', () => {
+          localStorage.setItem('selectedCarton', JSON.stringify(carton));
+          window.location = '/Bingo-sauro/tienda/comprarCarton.html';
+        });
+      } else {
+        precioBoton.textContent = `Carton comprado `;
+        precioBoton.classList.add('comprado-carton');
 
-      precioBoton.addEventListener('click', () => {
-        localStorage.setItem('selectedCarton', JSON.stringify(carton));
-        window.location = '/Bingo-sauro/tienda/comprarCarton.html';
-      });
-      
+      }
+
       cartonDiv.appendChild(precioBoton);
       contenedorCartones.appendChild(cartonDiv);
     });
   })
 
-  .catch(error => console.error('error al cargar los cartones',error));
+  .catch(error => console.error('error al cargar los cartones', error));
