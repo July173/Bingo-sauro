bfc246025f81123f67d7ad886502b5c091c542ca
JULY <danielitha345ra@gmail.com>
2024-11-27 18:55:37 -0500

    cartones de tienda y crear sala traidos del php

--- crear_sala/js/unirse_partida.js
+++ crear_sala/js/unirse_partida.js
@@ -2,17 +2,21 @@
 function unirseAPartida() {
     const codigo = document.getElementById("codigoEntrada").value;
     const nombre = document.getElementById("nombreJugador").value;
-    const foto = "ruta/a/la/foto.jpg"; // O selecciona la URL de la foto
+    // const foto = "ruta/a/la/foto.jpg"; // O selecciona la URL de la foto
   
-    fetch("/Crearsala/php/unirsePartida.php", {
+    fetch(".php/unirse_partida.php", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: `codigo=${codigo}&nombre=${nombre}&foto=${foto}`,
+      body: `codigo=${codigo}&nombre=${nombre}`,
     })
       .then((response) => response.json())
       .then((data) => {
         if (data.success) {
           agregarJugadorAlDOM(data.nombre, data.foto);
+          document.getElementById('redirigirJuego').addEventListener('click', function(){
+            window.location = "./../juego_usuario/juego-usuario.php"
+        });
+        
         } else {
           alert(data.error);
         }
@@ -20,10 +24,10 @@ function unirseAPartida() {
       .catch((error) => console.error("Error:", error));
   }
   
-  function agregarJugadorAlDOM(nombre, foto) {
+  function agregarJugadorAlDOM(nombre) {
     const contenedorJugadores = document.getElementById("contenedorJugadores");
     const jugadorDiv = document.createElement("div");
     jugadorDiv.classList.add("jugador");
-    jugadorDiv.innerHTML = `<img src="${foto}" alt="${nombre}"><p>${nombre}</p>`;
+    jugadorDiv.innerHTML = `< alt="${nombre}"><p>${nombre}</p>`;
     contenedorJugadores.appendChild(jugadorDiv);
   }
