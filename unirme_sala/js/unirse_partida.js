4336546fbf7f678f104f056ba425ac0f77b01535
JULY <danielitha345ra@gmail.com>
2024-12-03 18:59:43 -0500

    unirse a sala en proceso

---
+++ unirme_sala/js/unirse_partida.js
@@ -0,0 +1,29 @@
+function unirseAPartida() {
+    const codigo = document.getElementById('codigo').value;
+
+    // Validar si el código está vacío
+    if (!codigo) {
+        alert("Por favor ingresa un código de partida.");
+        return;
+    }
+
+    console.log(codigo);
+
+    fetch('./php/unirse_partida.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `codigo=${encodeURIComponent(codigo)}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // Redirigir a otra pantalla
+            window.location.href = '../juego_usuario/juego-usuario.php'; // Cambia a la URL deseada
+        } else {
+            alert(data.error);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
